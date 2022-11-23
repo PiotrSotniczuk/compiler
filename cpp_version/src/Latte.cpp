5ed698508aab9eb3ce8b56a7d850a5435d7898c3
sotnii <sotnii@gmail.com>
2022-11-24 00:42:56 +0100

    male ale dziala

---
+++ cpp_version/src/Latte.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <string.h>
+#include <fstream>
+#include <string>
+#include "../bnfc/Parser.H"
+#include "../bnfc/Absyn.H"
+#include "../bnfc/Skeleton.H"
+#include <map>
+#include <filesystem>
+#include <cassert>
+
+namespace fs = std::filesystem;
+using namespace std;
+
+class MyVisitor : public Skeleton {
+    public:
+        void compile(Program *p){
+            p->accept(this);
+        }
+
+    private:        
+        void visitNoInit(NoInit *no_init){
+            cout << "No initi\n";
+            visitIdent(no_init->ident_);
+        }
+};
+
+
+int main(int argc, char **argv)
+{
+	if (argc != 2) {
+		cerr << "Bad number of args you can only pass the path to file\n";
+		exit(1);
+	}
+	
+ 	char *filepath = argv[1];
+	FILE *input = fopen(filepath, "r");
+	if (!input) {
+		cerr << "Can't open file!\n";
+		exit(1);
+	}
+
+	Program *parse_tree = pProgram(input);
+	if (!parse_tree) {
+		exit(1);
+	}
+
+    cerr << "OK\n";
+
+	MyVisitor *myV = new MyVisitor();
+
+    myV->compile(parse_tree);
+
+	return 0;
+}
