ac09334d7810bdbaf9b97c3ee9535324114231f6
sotnii <sotnii@gmail.com>
2022-12-29 11:42:49 +0100

    dodanie lib

--- cpp_version/lib/runtime.cpp
+++ cpp_version/lib/runtime.cpp
@@ -1,31 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+void printInt(int x) { cout << x; }
+
+void printString(string s) { cout << s; }
+
 void error() {
-  puts("runtime error");
+  cerr << "runtime error\n";
   exit(1);
 }
 
-void printInt(Int64 x) { printf("%lld\n", x); }
-
-void printString(String s) { puts(s); }
-
-void _free(Ptr ptr) {
-  fprintf(stderr, "FREEING %lld\n", (Int64)ptr);
-  free(ptr);
+int readInt() {
+  int x;
+  cin >> x;
+  return x;
 }
 
-Int64 readInt() {
-  Int64 result;
-  scanf("%lld  ", &result);
-  return result;
-}
-
-String readString() {
-  String res = NULL;
-  size_t n = 0;
-  getline(&res, &n, stdin);
-  size_t len = strlen(res);
-  if (len > 0 && res[len - 1] == '\n') {
-    res[len - 1] = '\0';
-  }
-  _gcIncr(res);
-  return res;
+string readString() {
+  string str;
+  cin >> str;
+  return str;
 }
